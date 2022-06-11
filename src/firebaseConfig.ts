59f0a4e5d3096ec27f550386c144029dd998adeb
glopezfrutos <gus.lopezfrutos@gmail.com>
2022-06-11 17:10:12 -0300

    ADD ---  Firebase hosting setup

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBJQ6mvO0KLP4Qz_w67ls94Z4gFbU6gVK4",
+  authDomain: "donraulshardwarestore.firebaseapp.com",
+  projectId: "donraulshardwarestore",
+  storageBucket: "donraulshardwarestore.appspot.com",
+  messagingSenderId: "1037902115820",
+  appId: "1:1037902115820:web:87a1ae7b0ffed839d186c9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
